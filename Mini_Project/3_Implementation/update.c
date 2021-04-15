82647a86b3bfb2541556483a0c1f8fe4cf1f8314
anvithareddy <517anureddy@gmail.com>
2021-04-15 11:03:37 +0530

    recent

--- Mini_Project/3_Implementation/update.c
+++ Mini_Project/3_Implementation/update.c
@@ -1,2 +1,44 @@
 #include "function.h"
 
+void updateBook(){
+	int bookid,chooseCatg,flag=0;;
+	printf("\n\n\t\t\tENTER BOOK ID :");
+	scanf("%d",&bookid);
+    printf("\n\t\t\t----------------------------------------------\n");
+	
+	for(int i=0;i<count;i++){
+		if(book[i].books_id==bookid){
+			flag=1;
+			printf("\n\t\t\tBOOK ID%d",book[i].books_id);
+			printf(" AVAILABLE....");
+			printf("\n\t\t\tEnter new name:");
+			scanf("%s", book[i].bookName);
+			printf("\n\t\t\tEnter new author:");
+			scanf("%s", book[i].authorName);
+			printf("\n\t\t\tEnter new catagory = ");
+	        printf("\n\t\t\t1.History\n\t\t\t2.Arts_Music\n\t\t\t3.Biograhies\n\t\t\t4.Bussiness\n\t\t\t5.Comics\n\t\t\t6.Computer_Tech\n\t\t\t");
+	        scanf("%d",&chooseCatg);
+			switch(chooseCatg){
+				case 1:strcpy(book[i].catagory,"History");
+		        break;
+		        case 2:strcpy(book[i].catagory,"Arts_Music");
+		        break;
+		        case 3:strcpy(book[i].catagory,"Biograhies");
+		        break;
+		        case 4:strcpy(book[i].catagory,"Bussiness");
+		        break;
+		        case 5:strcpy(book[i].catagory,"Comics");
+		        break;
+		        case 6:strcpy(book[i].catagory,"Computer_Tech");
+		        break;
+		        default:strcpy(book[i].catagory,"Others");
+				}
+			printf("\n\t\t\tEnter new no pages = ");
+	        scanf("%d", &book[i].pages);
+            printf("\n\t\t\tEnter new price = ");
+	        scanf("%f", &book[i].price);
+		}
+	}
+	if(flag==0)
+		printf("\n\t\t\tBOOK NOT AVAILABLE");
+}
