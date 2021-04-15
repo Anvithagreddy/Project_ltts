dc4f15b857d82244f9a3a0cd433698917f477b4b
anvithareddy <517anureddy@gmail.com>
2021-04-15 13:36:40 +0530

    commit

--- Mini_Project/3_Implementation/inc/function.h
+++ Mini_Project/3_Implementation/inc/function.h
@@ -1,3 +1,13 @@
+/**
+ * @file function.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
 
@@ -8,7 +18,10 @@
 #define MAX_BOOK_NAME   50
 #define MAX_AUTHOR_NAME 50
 #define MAX_CATAGORY_NAME 50
-
+/**
+ * @brief 
+ * 
+ */
 struct booksInfo
 {
     unsigned int books_id; 
@@ -21,11 +34,38 @@ struct booksInfo
 
 struct booksInfo book[10];
 static int count=0;
-
+/**
+ * @brief 
+ * 
+ */
 void init();
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
 int addBook();
+/**
+ * @brief 
+ * 
+ */
 void searchBooks();
+/**
+ * @brief 
+ * 
+ */
 void viewBooks();
+/**
+ * @brief 
+ * 
+ */
 void updateBook();
+/**
+ * @brief 
+ * 
+ * @param cat_ty 
+ * @param count 
+ * @return int 
+ */
 int noOfBooksByCatagory(char cat_ty[],int count);
 #endif
